332e55c9e4af9af450cb62e9e12d4e862f8bf6c3
Jitesh Khurana <jkhurana_be21@thapar.edu>
2024-02-09 16:08:15 +0530

    made events page

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,7 +1,95 @@
-import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ComboBox } from "@/components/ComboBox";
+import { Badge } from "@/components/ui/badge";
+import Event from "../assets/event.png";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 
 const Events = () => {
-  return <div>Events</div>;
+  return (
+    <div className="m-10">
+      <div>
+        <Tabs defaultValue="upcoming" className="w-full">
+          <TabsList className="mt-5 mx-5">
+            <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
+            <TabsTrigger value="past">Past</TabsTrigger>
+          </TabsList>
+          <ComboBox />
+          <TabsContent value="upcoming">
+            <div className="flex flex-wrap justify-center">
+              {Array(12).fill(
+                <Card className="w-1/4 m-5">
+                  <img src={Event} className="h-44" />
+                  <CardHeader className="flex flex-row space-x-5">
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <div>
+                      <CardTitle>Escalade</CardTitle>
+                      <CardDescription>
+                        Creative Computing Society
+                      </CardDescription>
+                    </div>
+                  </CardHeader>
+                  <CardContent>
+                    <Badge variant="outline">#Engineering</Badge>
+                    <Badge variant="outline">#Coffee</Badge>
+                    <Badge variant="outline">#Coding</Badge>
+                  </CardContent>
+                  <CardFooter className="justify-between">
+                    <Button variant={"outline"} className="w-2/5">
+                      Preview
+                    </Button>
+                    <Button className="w-2/5">Apply</Button>
+                  </CardFooter>
+                </Card>
+              )}
+            </div>
+          </TabsContent>
+          <TabsContent value="past">
+            <div className="flex flex-wrap justify-center">
+              {Array(12).fill(
+                <Card className="w-1/4 m-5">
+                  <img src={Event} className="h-44" />
+                  <CardHeader className="flex flex-row space-x-5">
+                    <Avatar>
+                      <AvatarImage src="https://github.com/shadcn.png" />
+                      <AvatarFallback>CN</AvatarFallback>
+                    </Avatar>
+                    <div>
+                      <CardTitle>Hackathon</CardTitle>
+                      <CardDescription>MLSC</CardDescription>
+                    </div>
+                  </CardHeader>
+                  <CardContent>
+                    <Badge variant="outline">#Engineering</Badge>
+                    <Badge variant="outline">#Coffee</Badge>
+                    <Badge variant="outline">#Coding</Badge>
+                  </CardContent>
+                  <CardFooter className="justify-between">
+                    <Button variant={"outline"} className="w-2/5">
+                      Preview
+                    </Button>
+                    <Button className="w-2/5">Apply</Button>
+                  </CardFooter>
+                </Card>
+              )}
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default Events;
--- src/pages/Society.tsx
+++ src/pages/Society.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Creative from "../assets/creative.png";
 
 const Society = () => {
-  return <div>Society</div>;
+  return (
+    <div>
+      <h1 className="mx-10 my-5 text-3xl font-semibold">
+        Explore Societies @TIET
+      </h1>
+      <Card className="w-1/4">
+        <CardHeader>
+          <CardTitle>
+            <img src={Creative} />
+          </CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Creative Computing Society</p>
+        </CardContent>
+        <CardFooter>
+          <Button>Preview</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default Society;
