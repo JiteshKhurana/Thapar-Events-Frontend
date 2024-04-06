4d88796f026c0008488f46271afe974055286a3a
Jitesh Khurana <jkhurana_be21@thapar.edu>
2024-04-07 01:46:23 +0530

    FIX: SpeedInsights error

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import HomeNav from "./components/HomeNav";
 import { Toaster } from "@/components/ui/sonner";
 import { Outlet } from "react-router-dom";
 import { Provider } from "react-redux";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import store from "./store/store.ts";
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <HomeNav />
       <Outlet />
       <Toaster />
+      <SpeedInsights />
     </Provider>
   );
 }
