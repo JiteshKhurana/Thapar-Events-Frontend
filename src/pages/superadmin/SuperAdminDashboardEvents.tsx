331c3956f1c5b7c894f0e96860a03ea5addd694d
Jitesh Khurana <jkhurana_be21@thapar.edu>
2024-04-22 01:00:47 +0530

    Added private events to superadmin dashboard

--- src/pages/superadmin/SuperAdminDashboardEvents.tsx
+++ src/pages/superadmin/SuperAdminDashboardEvents.tsx
@@ -24,14 +24,33 @@ import { useEffect, useState } from "react";
 import { BiLinkExternal } from "react-icons/bi";
 import { useSelector } from "react-redux";
 import { Event } from "@/store/eventSlice";
+import { API_ENDPOINT } from "@/lib/constants";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import { toast } from "sonner";
 
 const SuperAdminDashboardEvents = () => {
   useEvents();
-
+  const cookies = new Cookies(null, { path: "/" });
+  const token = cookies.get("token");
   const Events = useSelector((store: RootState) => store.events.eventsList);
   const [upcomingEvents, setUpcomingEvents] = useState<Event[] | null>(null);
   const [pastEvents, setPastEvents] = useState<Event[] | null>(null);
+  const [privateEvents, setPrivateEvents] = useState<Event[] | null>(null);
+  const getPrivateEvents = async () => {
+    await axios
+      .get(API_ENDPOINT + "event/get/notvisible", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => setPrivateEvents(res.data))
+      .catch((error) => {
+        toast(error);
+      });
+  };
   useEffect(() => {
+    getPrivateEvents();
     if (Events) {
       const { upcomingEvents, pastEvents } = Events.reduce(
         (
@@ -73,6 +92,7 @@ const SuperAdminDashboardEvents = () => {
                   <TabsTrigger value="all">All</TabsTrigger>
                   <TabsTrigger value="upcoming">Upcoming</TabsTrigger>
                   <TabsTrigger value="past">Past</TabsTrigger>
+                  <TabsTrigger value="private">Private</TabsTrigger>
                 </TabsList>
               </div>
             </div>
@@ -272,6 +292,93 @@ const SuperAdminDashboardEvents = () => {
                     <TableBody>
                       {pastEvents &&
                         pastEvents.map((event) => {
+                          return (
+                            <TableRow key={event._Eid}>
+                              <TableCell className="hidden sm:table-cell">
+                                <img
+                                  src={event.image}
+                                  alt="Event image"
+                                  className="aspect-square rounded-md object-cover"
+                                  height="64"
+                                  width="64"
+                                />
+                              </TableCell>
+                              <TableCell className="font-medium">
+                                {event.title}
+                              </TableCell>
+                              <TableCell className="font-medium">
+                                {event.soc_name}
+                              </TableCell>
+                              <TableCell>
+                                <Badge
+                                  className={`${
+                                    event.visibility === "true"
+                                      ? "text-green-500"
+                                      : "text-red-500"
+                                  }`}
+                                  variant="outline"
+                                >
+                                  {event.visibility === "true"
+                                    ? "Public"
+                                    : "Private"}
+                                </Badge>
+                              </TableCell>
+                              <TableCell className="hidden md:table-cell">
+                                {timeConverter(event.start_date, true)}
+                              </TableCell>
+                              <TableCell className="hidden md:table-cell">
+                                {timeConverter(event.end_date, true)}
+                              </TableCell>
+                              <TableCell>
+                                <Badge variant="outline">
+                                  {event.event_type}
+                                </Badge>
+                              </TableCell>
+                              <TableCell>
+                                <Badge variant="outline">
+                                  {event.event_mode}
+                                </Badge>
+                              </TableCell>
+                              <TableCell>{event.venue}</TableCell>
+                              <TableCell>
+                                <Button
+                                  onClick={() => {}}
+                                  className="flex items-center gap-1"
+                                >
+                                  <BiLinkExternal /> Society Dashboard
+                                </Button>
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })}
+                    </TableBody>
+                  </Table>
+                </CardContent>
+              </Card>
+            </TabsContent>
+            <TabsContent value="private">
+              <Card x-chunk="dashboard-06-chunk-1">
+                <CardContent>
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="hidden w-[100px] sm:table-cell">
+                          <span className="hidden">Image</span>
+                        </TableHead>
+                        <TableHead>Event Name</TableHead>
+                        <TableHead>Society Name</TableHead>
+                        <TableHead>Visibility</TableHead>
+                        <TableHead>Start Date</TableHead>
+                        <TableHead>End Date</TableHead>
+                        <TableHead>Event Type</TableHead>
+                        <TableHead>Event Mode</TableHead>
+                        <TableHead>Venue</TableHead>
+                        <TableHead>Event Dashboard</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {privateEvents &&
+                        privateEvents.map((event) => {
                           return (
                             <TableRow key={event._Eid}>
                               <TableCell className="hidden sm:table-cell">
